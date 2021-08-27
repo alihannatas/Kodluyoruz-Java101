0e32f567a6e04bff4daabcc515c751a54805aded
Alihan Atas <alihan2644@gmail.com>
2021-08-27 04:01:13 +0300

    Pratical 4 updated.

--- Pratical4-TaxiFareCalculator.java
+++ Pratical4-TaxiFareCalculator.java
@@ -6,14 +6,17 @@ public class TaxiFareCalculator{
     public static void main(String[] args){
 
 
-        Scanner input = new Scanner(System.in);
+         Scanner input = new Scanner(System.in);
 
-              double perKilometer=2.2, amount=20,taximeterStart=10,wayTaken;
+              double perKilometer=2.2, amount=0,taximeterStart=10,wayTaken;
+
 
               System.out.print("What is the distance traveled:");
               wayTaken=input.nextDouble();
 
               amount+=  (wayTaken * perKilometer) + taximeterStart;
+
+              amount=(amount <20) ?  20:amount;
               System.out.println("Taxi amount="+ amount);
 
 
