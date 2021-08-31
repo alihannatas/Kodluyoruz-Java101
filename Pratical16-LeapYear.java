a819c82374295ab291d93a251af9c1389fe662ef
Alihan Atas <alihan2644@gmail.com>
2021-08-31 13:37:01 +0300

    Pratical16-LeapYear added

---
+++ Pratical16-LeapYear.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class LeapYear{
+
+
+    public static void main(String[] args)     {
+
+        Scanner input = new Scanner(System.in);
+
+        int yil;
+
+        System.out.print("Yıl Giriniz :");
+        yil=input.nextInt();
+
+        if(yil%400==0 || yil%4==0){
+
+            System.out.println(yil + " bir artık yıldır !");
+        }
+        else {
+            System.out.println(yil+ " bir artık yıldır değildir !");
+        }
+
+
+
+
+    }
+}
+
+
+
