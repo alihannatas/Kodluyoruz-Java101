e4cc2e44d15389d67e72ebf02d9caf7a9bb29173
Alihan Atas <alihan2644@gmail.com>
2021-09-01 02:08:30 +0300

    Praticel23-HarmonicNumbers added.

---
+++ Pratical23-HarmonicNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HarmoniNumbers{
+
+    public static void main(String[] args)     {
+
+        Scanner input = new Scanner(System.in);
+
+
+
+        System.out.print("N değerini giriniz : ");
+        int n = input.nextInt();
+        double result =0;
+        for (double i =1; i <=n; i++){
+            result += (1/i);
+        }
+        System.out.println(result);
+
+
+    }
+}
+
+
+
