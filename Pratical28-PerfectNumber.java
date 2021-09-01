dc487f25f69cd8a96dd8b3b9ac0cee2424e8ff51
Alihan Atas <alihan2644@gmail.com>
2021-09-01 19:40:04 +0300

    Pratical28-PerfectNumber added

---
+++ Pratical28-PerfectNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PerfectNumber{
+
+    public static void main(String[] args)     {
+
+        Scanner input = new Scanner(System.in);
+
+        int sayi,toplam=0;
+        System.out.print("Bir sayi giriniz:");
+        sayi=input.nextInt();
+
+            for(int i=1;i<sayi;i++){
+
+                if(sayi%i==0){
+
+                    toplam+=i;
+                }
+            }
+            if(sayi==toplam){
+            System.out.println(sayi + " Mükemmel sayıdır.");
+            }
+            else{
+                    System.out.println(sayi + " Mükemmel sayı değildir.");
+                }
+        }
+    }
+
+
+
+
