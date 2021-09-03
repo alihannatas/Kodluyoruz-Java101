32d3e236d2dfd0d9554853ad3f9035a88731a149
Alihan Atas <alihan2644@gmail.com>
2021-09-03 17:22:42 +0300

    Pratical37-MethodPattern added

---
+++ Pratical37-MethodPattern.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MethodPattern{
+
+        static void desen(int a,int b, boolean durum){
+
+                    if(a<=b) {
+                        System.out.println(a);
+                        if (a>0 && !durum) {
+                            desen(a-5,b,false);
+                        } else {
+                            desen(a + 5,b, true);
+                        }}}
+
+        public static void main(String[] args)     {
+
+        Scanner input = new Scanner(System.in);
+        int a=input.nextInt();
+
+        int b = a;
+
+            desen(a,b,false);
+
+        }
+    }
